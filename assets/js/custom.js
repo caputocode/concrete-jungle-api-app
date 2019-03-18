c83003dfb158996444cf47c344cb1771762385f8
Heather Caputo <caputoheather@ymail.com>
2019-03-18 13:03:40 +0000

    added custom.js file for hiding 'to top btn' until user scrolls down page

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+// Function hiding back to top button and making visible 20px from the top of the document, when user scrolls
+
+window.onscroll = function() { 
+        onScroll() 
+};
+    
+function onScroll() {
+    if (document.body.scrollTop > 310 || document.documentElement.scrollTop > 310) {
+            document.getElementById("toTop").style.display = "block";
+    }
+    else {
+        document.getElementById("toTop").style.display = "none";
+    }
+}
